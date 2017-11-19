9386898c79b06de5293221976d821c2af2960930
Akshita-Gawande <33800596+Akshita-Gawande@users.noreply.github.com>
2017-11-19 10:48:06 -0500

    Update shopping-site.sql

--- MySql-Database/shopping-site.sql
+++ MySql-Database/shopping-site.sql
@@ -1,7 +1,7 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: shopfree
--- ------------------------------------------------------
+-- -------------------------------------------------------
 -- Server version	5.7.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
