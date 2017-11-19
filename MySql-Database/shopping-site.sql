de93ac2f70dcd66b66dda5ddb30e144ded6b7926
Akshita-Gawande <33800596+Akshita-Gawande@users.noreply.github.com>
2017-11-19 10:32:16 -0500

    Update shopping-site.sql

--- MySql-Database/shopping-site.sql
+++ MySql-Database/shopping-site.sql
@@ -30,7 +30,7 @@ CREATE TABLE `Product_Cart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
+-- 
 -- Dumping data for table `Product_Cart`
 --
 
