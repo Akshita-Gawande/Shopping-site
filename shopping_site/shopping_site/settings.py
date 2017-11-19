d018d0ea2f95f3e66d1c38b4f7b8e389c0b657fe
vinod.ad <vinod.ad@MMMDVVINODAD.local>
2017-11-19 14:20:45 +0530

    changes in settings.py

--- shopping_site/shopping_site/settings.py
+++ shopping_site/shopping_site/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shopfree',
         'USER': 'root',
-        'PASSWORD': 'Vinod@123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
